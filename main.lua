b06760cadb9d2a8be88ec065625ee4b0b15746a8
jammin-ben <benjamin.reber19@houghton.edu>
2018-11-06 10:30:22 -0500

    Centered player origin

--- main.lua
+++ main.lua
@@ -15,6 +15,6 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.draw (Player.animation.spriteSheet, Player.animation.quads[Player.frame], Player.x,Player.y,Player.angle,1)
-    love.graphics.draw (Player2.animation.spriteSheet, Player2.animation.quads[Player2.frame], Player2.x,Player2.y,Player2.angle,1)
+    love.graphics.draw (Player.animation.spriteSheet, Player.animation.quads[Player.frame], Player.x,Player.y,Player.angle,1,1,16,16)
+    love.graphics.draw (Player2.animation.spriteSheet, Player2.animation.quads[Player2.frame], Player2.x,Player2.y,Player2.angle,1,1,16,16)
 end
