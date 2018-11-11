170b081b2dad02a9e0748e265aea73184ddcbfb3
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-11-10 20:43:09 -0500

    middle class thing done

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
+class = require('lib/middleclass/middleclass')
 require "animation_handler"
 require "player" 
+require 'scene'
 
 function love.load()
     world = love.physics.newWorld(0, 0, true)
@@ -24,7 +26,6 @@ function postSolve(a, b, coll, normalimpulse, tangentimpulse)
     -- print("postSolve")
 end
 
-
 function love.update(dt)
     world:update(dt)
     Player:step(dt)
@@ -32,6 +33,12 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.draw (Player.animation.spriteSheet, Player.animation.quads[Player.frame], Player.x,Player.y,Player.angle,1,1,64,64)
-    love.graphics.draw (Player2.animation.spriteSheet, Player2.animation.quads[Player2.frame], Player2.x,Player2.y,Player2.angle,1,1,64,64)
+    love.graphics.draw(Player.animation.spriteSheet, Player.animation.quads[Player.frame], Player.x,Player.y,Player.angle,1,1,64,64)
+    love.graphics.draw(Player2.animation.spriteSheet, Player2.animation.quads[Player2.frame], Player2.x,Player2.y,Player2.angle,1,1,64,64)
+end
+
+function love.keypressed(key)
+    if (key) == 'escape' then
+        love.event.quit()
+    end
 end
---
+++ scene.lua
@@ -0,0 +1,6 @@
+Scene = class('Scene')
+
+function Scene:init()
+	self.children = {}
+	print('scene has been created')
+end
