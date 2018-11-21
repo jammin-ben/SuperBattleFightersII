e664fca081518167e3c1f3cd87d41be6a2b35807
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-11-21 01:25:59 -0500

    cleaned up a little bit of minotaur movement code

--- Bull.lua
+++ Bull.lua
@@ -2,14 +2,15 @@ local class = require 'lib/middleclass'
 require 'player'
 Bull = Character:subclass('Bull')
 
-function Bull:initialize(world,x,y,speed,up,down,left,right,sprite,size)
-    Character.initialize(self,world,x,y,speed,up,down,left,right,sprite,size)
+function Bull:initialize(world, x, y, speed, up, down, left, right, sprite, size)
+    Character.initialize(self, world, x, y, speed, up, down, left, right, sprite, size)
     self.direction = 0
     self.turnSpeed = 3
+    -- self.turnSpeed = 40
 end
 function Bull:move(dt)
     self:rotate(dt)
-    Character.move(self,dt)
+    Character.move(self, dt)
 end 
 
 function Bull:rotate(dt)
@@ -18,7 +19,8 @@ function Bull:rotate(dt)
     if(self.angle <  0) then self.angle = self.angle + 2*math.pi end
     --turn 
     if(math.abs(self.angle - self.direction) > self.turnSpeed*dt) then 
-        if ((self.direction > self.angle and self.direction - self.angle <= math.pi) or (self.angle > self.direction and self.angle -self.direction > math.pi))then 
+				if ((self.direction > self.angle and self.direction - self.angle <= math.pi)
+				or (self.angle > self.direction and self.angle -self.direction > math.pi))then 
             self.angle = self.angle + self.turnSpeed*dt 
         else
             self.angle = self.angle - self.turnSpeed*dt 
@@ -29,36 +31,57 @@ function Bull:rotate(dt)
     end
 end
 
---Violates DRY (dont repeat yourself) 
-function Bull:checkForInput(dt)  --This function is designed to make movement feel bigger for the bull character, with turn speed accounted for
+--This function is designed to make movement feel bigger for the bull character,
+--with turn speed accounted for
+function Bull:checkForInput(dt)
+
+    local ANGLE_UP_RIGHT = math.pi * 7 / 4
+    local ANGLE_UP_LEFT = math.pi * 5 / 4
+    local ANGLE_DOWN_RIGHT = math.pi / 4
+    local ANGLE_DOWN_LEFT = math.pi * 3 / 4
+    local ANGLE_DOWN = math.pi / 2
+    local ANGLE_UP = 3 * math.pi / 2
+    local ANGLE_RIGHT = 0
+    local ANGLE_LEFT = math.pi
+
+    -- RIGHT_X
     if love.keyboard.isDown(self.right) then 
+        -- RIGHT_UP
         if love.keyboard.isDown(self.up) then 
-            self.direction = math.pi*7/4
+            self.direction = ANGLE_UP_RIGHT
             self:move(dt)
+        -- RIGHT_DOWN
         elseif love.keyboard.isDown(self.down) then
-            self.direction = math.pi /4
+            self.direction = ANGLE_DOWN_RIGHT
             self:move(dt)
+        -- RIGHT
         else 
-            self.direction = 0
+            self.direction = ANGLE_RIGHT
             self:move(dt)
         end 
             
+    -- LEFT_X
     elseif love.keyboard.isDown(self.left) then 
+        -- LEFT_UP
         if love.keyboard.isDown(self.up) then 
-            self.direction = math.pi*5/4
+            self.direction = ANGLE_UP_LEFT
             self:move(dt)
+        -- LEFT_DOWN
         elseif love.keyboard.isDown(self.down) then
-            self.direction = math.pi *3/4
+            self.direction = ANGLE_DOWN_LEFT
             self:move(dt)
+        -- LEFT
         else 
-            self.direction = math.pi
+            self.direction = ANGLE_LEFT
             self:move(dt)
         end
+    -- DOWN
     elseif love.keyboard.isDown(self.down) then
-        self.direction = math.pi/2
+        self.direction = ANGLE_DOWN
         self:move(dt)
+    -- UP
     elseif love.keyboard.isDown(self.up) then 
-        self.direction = 3*math.pi/2
+        self.direction = ANGLE_UP
         self:move(dt)
     end 
         
